14cee5eca68e44322a3691f98ca1e34401c404ed
Amin Dandache <ad@induux.com>
2016-03-16 14:19:59 +0100

    remove add_related_name, field is removed

--- social/apps/django_app/default/migrations/0003_merge.py
+++ social/apps/django_app/default/migrations/0003_merge.py
@@ -8,7 +8,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('default', '0002_auto_20150227_1432'),
-        ('default', '0002_add_related_name'),
     ]
 
     operations = [
